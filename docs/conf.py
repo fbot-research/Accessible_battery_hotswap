e109b2ac2705c63fa38bea4fa9921a3d3e049400
Gabrieltlt <gabrieltlt721@gmail.com>
2026-04-14 12:12:22 -0300

    :construction: first commit

---
+++ docs/conf.py
@@ -0,0 +1,62 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Accessible Battery Hotswap'
+copyright = '2026, FBOT@Research'
+author = 'FBOT@Research'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.youtube'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme_options = {
+    # --- Configuração das Logos ---
+    "light_logo": "team_images/logo_white.png", # O nome do seu arquivo para o tema claro
+    "dark_logo": "team_images/logo_black.png",  # O nome do seu arquivo para o tema escuro
+
+    # --- Cores personalizadas para o modo claro ---
+    "light_css_variables": {
+        "color-brand-primary": "#2980b9", 
+        "color-brand-content": "#2980b9", 
+    },
+    
+    # --- Cores personalizadas para o modo escuro ---
+    "dark_css_variables": {
+        "color-brand-primary": "#ff5500", 
+        "color-brand-content": "#ff5500", 
+    },
+    
+    "sidebar_hide_name": True,
+}
+
+html_js_files = [
+    "theme.js",
+]
+
+html_title = ""
+html_theme = 'furo'
+html_permalinks = False
+html_static_path = ['_static']
+html_css_files = ["css/custom.css"]
+html_favicon = "_static/team_images/favicon.ico"
